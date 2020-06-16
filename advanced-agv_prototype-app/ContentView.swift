f7c0e9b24d1327c07c05c99ce24ed52c761dfe01
Lukas Gehrke <lukas.gehrke@t-online.de>
2020-06-16 15:57:52 +0200

    Initial commit.

---
+++ advanced-agv_prototype-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  advanced-agv_prototype-app
+//
+//  Created by Lukas Gehrke on 16.06.20.
+//  Copyright © 2020 Lukas Gehrke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

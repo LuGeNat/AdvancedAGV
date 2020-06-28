9802cc356cf80eecb6ee4a73c1fcb5932c0e9523
Lukas Gehrke <lukas.gehrke@t-online.de>
2020-06-28 19:25:51 +0200

    Changes voltage values from int to float.

--- advanced-agv_prototype-app/Model/Agv.swift
+++ advanced-agv_prototype-app/Model/Agv.swift
@@ -16,27 +16,27 @@ struct Agv: Identifiable {
     let key: String
     let name: String
     let temperature: Int
-    let voltage: Int
+    let voltage: Double
     let timestamp: String
     
-    init(name: String, temperature: Int, voltage: Int, timestamp: String, key: String = "") {
+    init(name: String, temperature: Int, voltage: Double, timestamp: String, key: String = "") {
         self.id = UUID()
         self.ref = nil
         self.key = ""
         self.name = name
         self.temperature = temperature
-        self.voltage = voltage
+        self.voltage = Double(round(10*voltage)/10)
         self.timestamp = timestamp
     }
     
     init?(snapshot: DataSnapshot) {
-        guard let value = snapshot.value as? [String: AnyObject], let name = value["name"] as? String, let temperature = value["temperature"] as? Int, let voltage = value["voltage"] as? Int, let timestamp = value["timestamp"] as? String else { return nil }
+        guard let value = snapshot.value as? [String: AnyObject], let name = value["name"] as? String, let temperature = value["temperature"] as? Int, let voltage = value["voltage"] as? Double, let timestamp = value["timestamp"] as? String else { return nil }
         self.id = UUID()
         self.ref = snapshot.ref
         self.key = snapshot.key
         self.name = name
         self.temperature = temperature
-        self.voltage = voltage
+        self.voltage = Double(round(10*voltage)/10)
         self.timestamp = timestamp
     }
     
